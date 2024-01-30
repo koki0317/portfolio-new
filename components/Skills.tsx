e6ca2c4db187403da8a96670b69dfd61b5b404a2
Koki-T3 <koki.takahashi@scalefast.com>
2024-01-30 15:06:51 +0900

    Skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,20 +1,35 @@
 "use client";
 
-import {
-  Backend_skill,
-  Frontend_skill,
-  Full_stack,
-  Other_skill,
-  Skill_data,
-} from "@/constants";
+import { Skill_data } from "@/constants";
 import React from "react";
 import SkillDataProvider from "./SkillDataProvider";
 import SkillText from "./SkillText";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+import NextJs from "@/public/skills/nextjs.svg";
+import HTML from "@/public/skills/html.svg";
+
+import { useTheme } from "next-themes";
 
 const Skills = () => {
+  const { theme } = useTheme();
+
   return (
-    <section className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20">
-      Hello
+    <section className="px-4 lg:px-6 py-20">
+      <div className="">
+        <h2 className="title-text text-center mb-5">Skills</h2>
+        <div className="flex flex-wrap gap-3">
+          {Skill_data.map((skill) => (
+            <skill.src
+              width={32}
+              height={32}
+              key={skill.alt}
+              fill={theme === "dark" ? "white" : "black"}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
--- constants/index.ts
+++ constants/index.ts
@@ -1,246 +1,93 @@
+import Html from "@/public/skills/html.svg";
+import Css from "@/public/skills/css.svg";
+import Javascript from "@/public/skills/javascript.svg";
+import Typescript from "@/public/skills/typescript.svg";
+import Tailwind from "@/public/skills/tailwind.svg";
+import Nodejs from "@/public/skills/nodejs.svg";
+import React from "@/public/skills/react.svg";
+import Next from "@/public/skills/nextjs.svg";
+import Vue from "@/public/skills/vue.svg";
+import Nuxt from "@/public/skills/nuxt.svg";
+import Python from "@/public/skills/python.svg";
+import Markdown from "@/public/skills/markdown.svg";
+import Git from "@/public/skills/git.svg";
+import Figma from "@/public/skills/figma.svg";
+import Linux from "@/public/skills/linux.svg";
+import Ubuntu from "@/public/skills/ubuntu.svg";
+import Slack from "@/public/skills/slack.svg";
+import Trello from "@/public/skills/trello.svg";
+
 export const Skill_data = [
   {
-    skill_name: "Html 5",
-    Image: "/html.png",
-    width: 80,
-    height: 80,
+    src: Html,
+    alt: "HTML Logo",
   },
   {
-    skill_name: "Css",
-    Image: "/css.png",
-    width: 80,
-    height: 80,
+    src: Css,
+    alt: "HTML Logo",
   },
   {
-    skill_name: "Java Script",
-    Image: "/js.png",
-    width: 65,
-    height: 65,
+    src: Javascript,
+    alt: "Javascript Logo",
   },
   {
-    skill_name: "Tailwind Css",
-    Image: "/tailwind.png",
-    width: 80,
-    height: 80,
+    src: Typescript,
+    alt: "Typescript Logo",
   },
   {
-    skill_name: "React",
-    Image: "/react.png",
-    width: 80,
-    height: 80,
+    src: Nodejs,
+    alt: "Nodejs Logo",
   },
   {
-    skill_name: "Redux",
-    Image: "/redux.png",
-    width: 80,
-    height: 80,
+    src: React,
+    alt: "React Logo",
   },
   {
-    skill_name: "React Query",
-    Image: "/reactquery.png",
-    width: 80,
-    height: 80,
+    src: Next,
+    alt: "Next Logo",
   },
   {
-    skill_name: "Type Script",
-    Image: "/ts.png",
-    width: 80,
-    height: 80,
+    src: Vue,
+    alt: "Vue Logo",
   },
   {
-    skill_name: "Next js 13",
-    Image: "/next.png",
-    width: 80,
-    height: 80,
+    src: Nuxt,
+    alt: "Nuxt Logo",
   },
   {
-    skill_name: "Framer Motion",
-    Image: "/framer.png",
-    width: 80,
-    height: 80,
+    src: Tailwind,
+    alt: "Tailwind Logo",
   },
   {
-    skill_name: "Stripe Payment",
-    Image: "/stripe.webp",
-    width: 80,
-    height: 80,
+    src: Python,
+    alt: "Python Logo",
   },
   {
-    skill_name: "Node js",
-    Image: "/node-js.png",
-    width: 80,
-    height: 80,
+    src: Markdown,
+    alt: "Markdown Logo",
   },
   {
-    skill_name: "Mongo db",
-    Image: "/mongodb.png",
-    width: 40,
-    height: 40,
-  },
-];
-
-export const Socials = [
-  {
-    name: "Discord",
-    src: "/instagram.svg",
+    src: Git,
+    alt: "Git Logo",
   },
   {
-    name: "Facebook",
-    src: "/facebook.svg",
+    src: Figma,
+    alt: "Figma Logo",
   },
   {
-    name: "Instagram",
-    src: "/discord.svg",
-  },
-];
-
-export const Frontend_skill = [
-  {
-    skill_name: "Html 5",
-    Image: "/html.png",
-    width: 80,
-    height: 80,
+    src: Linux,
+    alt: "Linux Logo",
   },
   {
-    skill_name: "Css",
-    Image: "/css.png",
-    width: 80,
-    height: 80,
+    src: Ubuntu,
+    alt: "Ubuntu Logo",
   },
   {
-    skill_name: "Java Script",
-    Image: "/js.png",
-    width: 65,
-    height: 65,
+    src: Slack,
+    alt: "Slack Logo",
   },
   {
-    skill_name: "Tailwind Css",
-    Image: "/tailwind.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Material UI",
-    Image: "/mui.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "React",
-    Image: "/react.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Redux",
-    Image: "/redux.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "React Query",
-    Image: "/reactquery.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Type Script",
-    Image: "/ts.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Next js 13",
-    Image: "/next.png",
-    width: 80,
-    height: 80,
-  },
-];
-
-export const Backend_skill = [
-  {
-    skill_name: "Node js",
-    Image: "/node-js.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Express js",
-    Image: "/express.png",
-    width: 80,
-    height: 80,
-  },
-  {
-    skill_name: "Mongo db",
-    Image: "/mongodb.png",
-    width: 40,
-    height: 40,
-  },
-  {
-    skill_name: "Fire base",
-    Image: "/Firebase.png",
-    width: 55,
-    height: 55,
-  },
-  {
-    skill_name: "Postger SQL",
-    Image: "/postger.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "My SQL",
-    Image: "/mysql.png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "Prisma",
-    Image: "/prisma.webp",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "Graphql",
-    Image: "/graphql.png",
-    width: 80,
-    height: 80,
-  },
-];
-
-export const Full_stack = [
-  {
-    skill_name: "React Native",
-    Image: "/ReactNative .png",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "Tauri",
-    Image: "/tauri.svg",
-    width: 70,
-    height: 70,
-  },
-  {
-    skill_name: "Docker",
-    Image: "/docker.webp",
-    width: 70,
-    height: 70,
-  },
-
-  {
-    skill_name: "Figma",
-    Image: "/figma.png",
-    width: 50,
-    height: 50,
-  },
-];
-
-export const Other_skill = [
-  {
-    skill_name: "Go",
-    Image: "/go.png",
-    width: 60,
-    height: 60,
+    src: Trello,
+    alt: "Trello Logo",
   },
 ];
