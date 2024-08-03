90deb5aa719ce8a51f5aed2dc92c3e6fe88a204d
Koki-T3 <reunion.fy0317@gmail.com>
2024-08-03 10:45:30 +0900

    Improve accesability

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -7,6 +7,7 @@ export const MobileSidebar = () => {
   return (
     <Sheet>
       <SheetTrigger className="lg:hidden">
+        <span className="sr-only">Open mobile menu</span>
         <Menu className="text-gray-300" />
       </SheetTrigger>
       <SheetContent side="right" className="p-0 bg-secondary pt-10 w-32">
